cb46d3217ef81a7f86b5425628ffe3048b5892ff
apainintheneck <apainintheneck@gmail.com>
2024-01-20 21:48:46 -0800

    Initialize new repo

---
+++ spec/gitsh_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Gitsh do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/gitsh.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Gitsh`
+module Gitsh
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
