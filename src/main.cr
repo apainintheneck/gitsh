6783655196f92faa4e5fdd6f5d5fb6c538cbed1e
apainintheneck <apainintheneck@gmail.com>
2024-02-04 16:36:40 -0800

    Add nicer shell prompt
    This adds color, branch, uncommited and commited changes to the shell prompt.

--- src/main.cr
+++ src/main.cr
@@ -15,8 +15,49 @@ puts <<-WELCOME
 #############################################################
 WELCOME
 
+def build_prompt(branch : String? = nil, unstaged_changes : UInt32 = 0, staged_changes : UInt32 = 0) : String
+  String.build do |str|
+    str << "gitsh".colorize(:light_cyan).mode(:bold)
+    if branch
+      str << " (" << branch.colorize(:magenta).mode(:bold) << "|"
+
+      if unstaged_changes.zero? && staged_changes.zero?
+        str << "✔".colorize(:green).mode(:bold)
+      end
+
+      if staged_changes.positive?
+        Colorize.with.yellow.surround(str) do
+          str << "●" << staged_changes
+        end
+      end
+
+      if unstaged_changes.positive?
+        Colorize.with.blue.surround(str) do
+          str << "+" << unstaged_changes
+        end
+      end
+
+      str << ")"
+    end
+    str << "> "
+  end
+end
+
+DEFAULT_PROMPT = build_prompt
+
 loop do
-  line = Linenoise.prompt("gitsh> ").try(&.strip)
+  if Git.repo?
+    changes = Git.uncommitted_changes
+    prompt_str = build_prompt(
+      branch: Git.current_branch,
+      unstaged_changes: changes[:unstaged_count],
+      staged_changes: changes[:staged_count],
+    )
+  else
+    prompt_str = DEFAULT_PROMPT
+  end
+
+  line = Linenoise.prompt(prompt_str).try(&.strip)
   break if line.nil?
 
   args = Process.parse_arguments(line)
